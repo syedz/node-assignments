f55d029fc50a81e116b01ded6261a30016677ebc
Zeeshan Syed <zsyed89@gmail.com>
2023-11-23 07:01:35 -0500

    Setup student-summaries project, removed json-server module as it's no longer used.

---
+++ student-summaries/server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
